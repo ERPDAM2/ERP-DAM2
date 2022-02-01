af77e7678286801dc91b91bc4cb8c5c0a5a12bd6
JosiVarela <josi.varela18@gmail.com>
2022-02-01 11:43:29 +0100

    Configuracion de la estructura basica de rutas

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from routes.users import users
 
 PORT = 3000
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+app.register_blueprint(users)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello world</p>"
+def main_page():
+    return render_template("./index.html")
 
 if(__name__=="__main__"):
     app.run(port= PORT)
