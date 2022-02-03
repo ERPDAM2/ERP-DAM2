82106946a10650465d81b7f0c60b4dc483ba3be7
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-02-03 14:45:13 +0100

    format and config from class

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from config import ConfigTest
 from routes.users import users
 from routes.products import products
 from routes.clients import clients
@@ -14,9 +15,13 @@ app.register_blueprint(users)
 app.register_blueprint(products)
 app.register_blueprint(clients)
 
+app.config.from_object(ConfigTest())
+
+
 @app.route("/")
 def main_page():
     return render_template("./index.html")
 
-if(__name__=="__main__"):
-    app.run(port= PORT)
+
+if (__name__ == "__main__"):
+    app.run(port=PORT)
