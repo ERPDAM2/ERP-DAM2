6a2590fb34ba0fc0bf384f8c88e2f1695fd7e288
Iago <iagoblopez@gmail.com>
2022-02-03 12:39:17 +0100

    Validation Product

--- erpdam2/routes/products.py
+++ erpdam2/routes/products.py
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
+
 from flask import Blueprint, render_template
 from erpdam2.View.forms import RegisterProduct
+from erpdam2.models.user import Product
 
 products = Blueprint("products", __name__)
 
 @products.route("/products/add", methods=["GET", "POST"])
 def productsRoute():
     form = RegisterProduct()
+
+    if (form.validate_on_submit()):
+        product = Product(id=form.id_product.data, name=form.name_product.data, 
+                          description=form.description.data, quantity=form.quantity.data, 
+                          price=form.price.data, picture=form.picture.data)
+    
     
     return render_template("testAltaProductos.html", form=form, title="Alta producto")
-=======
-from turtle import title
-from flask import Blueprint, render_template
-from ..View.forms import RegisterSale
 
-products = Blueprint("products", __name__)
-
-
-@products.route("/sales", methods=["GET", "POST"])
-def sales():
-    form = RegisterSale()
-
-    return render_template("test.html", form=form, title="Sales")
->>>>>>> b88b761919efcb9365cf6bb24ead0fb2ce1e0f38
