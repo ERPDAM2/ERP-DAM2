ad235f7dd9b4a389a59d2062b4bed2fd2d94b09f
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-02-06 21:48:11 +0100

    fixed wrong type

--- models/rol.py
+++ models/rol.py
@@ -7,7 +7,7 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True)
     description = db.Column(db.String(200))
-    is_admin = db.Column(db.bool)
+    is_admin = db.Column(db.Boolean())
     users = db.relationship('User', backref='role',
                                 lazy='dynamic')
 
