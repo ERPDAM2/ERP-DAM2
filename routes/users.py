f18cc678b99be855dca6e0ddb6b2cc9cf68af61a
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-02-06 19:56:14 +0100

    fix routes

--- routes/users.py
+++ routes/users.py
@@ -5,7 +5,7 @@ users = Blueprint("users", __name__)
 @users.route("/register", methods=["GET", "POST"])
 def register():
     
-    if(request.method == "GET"):
+    if request.method == "GET":
         return "Prueba"
     
 @users.route("/login", methods=["GET", "POST"])
