fdbe57a841d3d679abc6d774f72e185bbeca4567
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-02-07 23:13:51 +0100

    product added

--- erpdam2/models/__init__.py
+++ erpdam2/models/__init__.py
@@ -2,3 +2,4 @@ from erpdam2.models.contact import Contact
 from erpdam2.models.role import Role
 from erpdam2.models.transaction import Transaction
 from erpdam2.models.user import User
+from erpdam2.models.product import Product
