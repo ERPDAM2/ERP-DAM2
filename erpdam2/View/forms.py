1725a9bd1165800dce13f08bb2bd7adebf421f8e
JosiVarela <josi.varela18@gmail.com>
2022-02-08 09:35:27 +0100

    Form class created

--- erpdam2/View/forms.py
+++ erpdam2/View/forms.py
@@ -2,6 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
 
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                                        DataRequired(),
+                                        EqualTo('confirm_password')
+                                        ])
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Register')
+    
 
 class LoginForm(FlaskForm):
     """
