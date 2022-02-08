eeb87ea83b43d426ef8013401919c09b9fe54ae3
raulas <raulalleguesalgado1@gmail.com>
2022-02-08 09:33:53 +0100

    Register of contact

--- erpdam2/View/forms.py
+++ erpdam2/View/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import PasswordField, StringField, SubmitField,EmailField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo,NumberRange
 
 
 class LoginForm(FlaskForm):
@@ -11,3 +11,24 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+
+
+class RegisterContact(FlaskForm):
+    """
+    Dar de alta contacto
+    """
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    phone = StringField("Phone", validators=[DataRequired(), NumberRange(min=111111111,max=999999999)])
+    picture = StringField("Email", validators=[DataRequired(), Email()])
+
+
+"""
+ self,
+        email,
+        phone,
+        picture,
+        address,
+        company_name=None,
+        first_name=None,
+        last_name=None,
+"""
