7c118d85171a4a0fa50a4e177696e9a537fe2b00
raulas <raulalleguesalgado1@gmail.com>
2022-02-08 10:51:33 +0100

    Validation of contact

--- erpdam2/routes/clients.py
+++ erpdam2/routes/clients.py
@@ -1,8 +1,39 @@
+from audioop import add
+import email
 from flask import Blueprint
+from turtle import title
+from flask import Blueprint, render_template, request,redirect,url_for
+from ..View.forms import RegisterContact
+from .. import db
+from . import auth
+from .. models import contact
 
 clients = Blueprint("clients", __name__)
 
 
 @clients.route("/clients", methods=["GET", "POST"])
 def clientsMethod():
-    return ""
+     form = RegisterContact()
+     if form.validate_on_submit():
+         contactP=contact(
+             email = form.email.data,
+             phone=form.phone.data,
+             picture=form.picture.data,
+             address=form.address.data,
+             company_name=form.company_name.data,
+             first_name=form.first_name.data,
+             last_name=form.last_name.data )
+        
+   
+             
+
+
+             
+
+
+         
+         
+    
+
+
+     return render_template("test.html", form=form, title="RegisterContact")
