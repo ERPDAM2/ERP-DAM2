92291f2cc5b1abbe32c8b0457c7229d4a2db9b90
JosiVarela <josi.varela18@gmail.com>
2022-02-08 09:37:56 +0100

    form class created

--- erpdam2/View/forms.py
+++ erpdam2/View/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField,EmailField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo,NumberRange
+from wtforms import PasswordField, StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -13,7 +13,7 @@ class RegisterForm(FlaskForm):
                                         ])
     confirm_password = PasswordField('Confirm Password')
     submit = SubmitField('Register')
-    
+     
 
 class LoginForm(FlaskForm):
     """
@@ -23,24 +23,3 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
-
-
-class RegisterContact(FlaskForm):
-    """
-    Dar de alta contacto
-    """
-    email = EmailField("Email", validators=[DataRequired(), Email()])
-    phone = StringField("Phone", validators=[DataRequired(), NumberRange(min=111111111,max=999999999)])
-    picture = StringField("Email", validators=[DataRequired(), Email()])
-
-
-"""
- self,
-        email,
-        phone,
-        picture,
-        address,
-        company_name=None,
-        first_name=None,
-        last_name=None,
-"""
