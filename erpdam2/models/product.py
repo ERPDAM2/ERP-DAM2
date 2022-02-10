43a754ff7ed106f69661761281de3ec6d82cc9af
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-02-10 09:14:35 +0100

    ProductCategory

--- erpdam2/models/product.py
+++ erpdam2/models/product.py
@@ -10,11 +10,13 @@ class Product(db.Model):
     quantity = db.Column(db.Integer)
     price = db.Column(db.Float, nullable=False)
     picture = db.Column(db.String(255))
+    category_id = db.Column(db.Integer, db.ForeignKey("product_category.id"))
     transactions = db.relationship("ProductTransaction", back_populates="product")
 
-    def __init__(self, name, description, quantity, price, picture):
+    def __init__(self, name, description, quantity, price, picture, category_id=None):
         self.name = name
         self.description = description
         self.quantity = quantity
         self.price = price
         self.picture = picture
+        self.category_id = category_id
